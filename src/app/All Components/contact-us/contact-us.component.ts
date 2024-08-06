5509e6d4ee3f63e9f4df43086001848173567441
NabeelAhmad <nabeelahmadkhan03@gmail.com>
2024-08-06 06:30:00 -0700

    complete contactus form

--- src/app/All Components/contact-us/contact-us.component.ts
+++ src/app/All Components/contact-us/contact-us.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrls: ['./contact-us.component.css'] 
 })
 export class ContactUsComponent {
+  contactForm: FormGroup =new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    street: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    city: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    postCode: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    phoneNo: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email,Validators.minLength(15)]),
+    message: new FormControl(''),
+    terms: new FormControl(false, [Validators.requiredTrue])
+  });
 
+  constructor(private fb: FormBuilder){}
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.contactForm.value);
+    this.contactForm.reset();
+  }
 }
