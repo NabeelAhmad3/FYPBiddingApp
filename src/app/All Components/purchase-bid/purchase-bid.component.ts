9fcb60c6bab95deff4643d9c6ad9b1de31081376
NabeelAhmad <nabeelahmadkhan03@gmail.com>
2024-08-11 08:55:50 -0700

    purchaseBid complete

--- src/app/All Components/purchase-bid/purchase-bid.component.ts
+++ src/app/All Components/purchase-bid/purchase-bid.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-purchase-bid',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './purchase-bid.component.html',
   styleUrl: './purchase-bid.component.css'
 })
 export class PurchaseBidComponent {
 
+summary=[
+ { serial:'1', productName:'Prado 2024',winingAmount:'3323',quantity: '1'},
+ { serial: '2', productName: 'Civic 2024', winingAmount: '2879', quantity: '2' },
+ { serial: '3', productName: 'Corolla 2024', winingAmount: '3100', quantity: '1' }
+]
 }
