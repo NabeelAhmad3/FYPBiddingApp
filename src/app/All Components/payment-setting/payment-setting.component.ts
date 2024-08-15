ace71ea6c941b5c1180dc0aa1f763327424bc5a2
NabeelAhmad <nabeelahmadkhan03@gmail.com>
2024-08-15 18:00:53 +0500

    add card to paymentSetting

--- src/app/All Components/payment-setting/payment-setting.component.ts
+++ src/app/All Components/payment-setting/payment-setting.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { AddCardComponent } from "../../pop-ups/add-card/add-card.component";
 import { AddMethodComponent } from "../../pop-ups/add-method/add-method.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-payment-setting',
   standalone: true,
-  imports: [AddCardComponent, AddMethodComponent],
+  imports: [AddCardComponent, AddMethodComponent,CommonModule],
   templateUrl: './payment-setting.component.html',
-  styleUrl: './payment-setting.component.css'
+  styleUrls: ['./payment-setting.component.css']
 })
 export class PaymentSettingComponent {
+  addedCards: any[] = []; 
 
+  onCardAdded(cardData: any) {
+    this.addedCards.push(cardData);
+  }
+  deleteCard(index: number) {
+    this.addedCards.splice(index, 1); 
+  }
 }
--- src/app/pop-ups/add-card/add-card.component.ts
+++ src/app/pop-ups/add-card/add-card.component.ts
@@ -1,29 +1,34 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-card',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-card.component.html',
-  styleUrl: './add-card.component.css'
+  styleUrls: ['./add-card.component.css']
 })
 export class AddCardComponent {
-  addCardForm: FormGroup =new FormGroup({
+  @Output() cardAdded = new EventEmitter<any>(); // Event emitter to send data to parent component
+
+  addCardForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     cardNumber: new FormControl('', [Validators.required, Validators.minLength(3)]),
     cvv: new FormControl('', [Validators.required, Validators.minLength(3)]),
     expiryDate: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
-  constructor(private fb: FormBuilder){}
+  formData: any = {};
+  constructor(private fb: FormBuilder) { }
+
   onSubmit() {
     if (this.addCardForm.invalid) {
       this.addCardForm.markAllAsTouched();
       return;
     }
-    console.log(this.addCardForm.value);
+    this.formData = this.addCardForm.value;
+    this.cardAdded.emit(this.formData); 
     this.addCardForm.reset();
   }
 }
